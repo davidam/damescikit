5dd920c65f28611a10d928822e354628e4df001e
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-04-08 20:45:04 +0200

    tests/test_models.py: add gaussiannb model

--- damescikit/tests/test_models.py
+++ damescikit/tests/test_models.py
@@ -73,3 +73,17 @@ class TddInPythonExample(unittest.TestCase):
         clf.fit(X, y)
         print(clf.score(X, y))
         self.assertEqual(1.0, clf.score(X,y))
+
+
+    def test_models_gaussiannb(self):
+        #assigning predictor and target variables
+        x= np.array([[-3,7],[1,5], [1,2], [-2,0], [2,3], [-4,0], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-2,7]])
+        y = np.array([3, 3, 3, 3, 4, 3, 3, 4, 3, 4, 4, 4])
+        #Create a Gaussian Classifier
+        model = GaussianNB()
+        # Train the model using the training sets
+        model.fit(x, y)
+        #Predict Output
+        predicted= model.predict([[1,2],[3,4]])
+#        print(predicted)
+        self.assertTrue(np.array_equal(predicted, np.array([3, 4])))
