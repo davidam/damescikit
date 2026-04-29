5de007321c2a14c11f788b1ee639623431601a7e
David Arroyo Menéndez <davidam@gmail.com>
2026-04-29 14:51:40 +0200

    tests/test_datasets.py: links to external documentation

--- damescikit/tests/test_datasets.py
+++ damescikit/tests/test_datasets.py
@@ -27,6 +27,8 @@ from sklearn.datasets import load_iris
 from sklearn.datasets import fetch_lfw_people
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.datasets import fetch_california_housing
+from sklearn.datasets import fetch_rcv1
+
 import collections
 collections.Callable = collections.abc.Callable
 
@@ -34,10 +36,12 @@ collections.Callable = collections.abc.Callable
 class TddInPythonExample(unittest.TestCase):
 
     def test_iris(self):
+        # https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_iris.html
         data = load_iris()
         self.assertEqual(len(data), 8)
 
-    def test_newsgroups(self):    
+    def test_newsgroups(self):
+        # https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_20newsgroups.html
         cats = ['alt.atheism', 'sci.space']
         newsgroups_train = fetch_20newsgroups(subset='train', categories=cats)
         a = list(newsgroups_train.target_names)
@@ -51,6 +55,7 @@ class TddInPythonExample(unittest.TestCase):
         self.assertTrue(np.array_equal(b,res2))
 
     def test_california(self):
+        # https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_20newsgroups.html
         housing = fetch_california_housing()
         # print(housing.data.shape, housing.target.shape)
         a = housing.feature_names[0:6]
@@ -58,6 +63,7 @@ class TddInPythonExample(unittest.TestCase):
         self.assertEqual(a, res1)
 
     def test_lfw(self):
+        # https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_lfw_people.html#sklearn.datasets.fetch_lfw_people
         lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
         n = len(lfw_people)
         res = 5
@@ -65,5 +71,5 @@ class TddInPythonExample(unittest.TestCase):
         name = lfw_people.target_names[0]
         res = "Ariel Sharon"
         self.assertEqual(name,res)
-
         
+
